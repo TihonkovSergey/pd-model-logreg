63927c8a8816ad79e0cd05926f3ebe77aa819293
TihonkovSergey <slezki.edinoroga@mail.ru>
2021-11-01 00:33:45 +0300

    Minor fixes

--- src/data/download.py
+++ src/data/download.py
@@ -1,6 +1,11 @@
 import os
 
-if __name__ == '__main__':
+
+def download_data():
     os.system("wget https://raw.githubusercontent.com/BKHV/risk_models/master/data/PD-data-train.csv -P data/raw/")
     os.system("wget https://raw.githubusercontent.com/BKHV/risk_models/master/data/PD-data-test.csv -P data/raw/")
     os.system("wget https://raw.githubusercontent.com/BKHV/risk_models/master/data/PD-data-desc.csv -P data/raw/")
+
+
+if __name__ == '__main__':
+    download_data()
