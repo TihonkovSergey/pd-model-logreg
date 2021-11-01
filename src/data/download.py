e15b75041c72deedf14ebe7b61e573d8f6c1725a
TihonkovSergey <slezki.edinoroga@mail.ru>
2021-11-01 03:29:02 +0300

    fixed path

--- src/data/download.py
+++ src/data/download.py
@@ -1,10 +1,13 @@
 import os
+from definitions import ROOT_DIR
+from pathlib import Path
 
 
 def download_data():
-    os.system("wget https://raw.githubusercontent.com/BKHV/risk_models/master/data/PD-data-train.csv -P data/raw/")
-    os.system("wget https://raw.githubusercontent.com/BKHV/risk_models/master/data/PD-data-test.csv -P data/raw/")
-    os.system("wget https://raw.githubusercontent.com/BKHV/risk_models/master/data/PD-data-desc.csv -P data/raw/")
+    data_path = Path(ROOT_DIR).joinpath("data/raw/")
+    os.system(f"wget https://raw.githubusercontent.com/BKHV/risk_models/master/data/PD-data-train.csv -P {data_path}")
+    os.system(f"wget https://raw.githubusercontent.com/BKHV/risk_models/master/data/PD-data-test.csv -P {data_path}")
+    os.system(f"wget https://raw.githubusercontent.com/BKHV/risk_models/master/data/PD-data-desc.csv -P {data_path}")
 
 
 if __name__ == '__main__':
