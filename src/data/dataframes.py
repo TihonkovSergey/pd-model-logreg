8cc232cd12a9d922872cbfc3351dfd4c31bec8dc
TihonkovSergey <slezki.edinoroga@mail.ru>
2021-11-01 03:28:05 +0300

    Added functions for loading dataframes

---
+++ src/data/dataframes.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from definitions import ROOT_DIR
+from pathlib import Path
+
+
+def get_train():
+    data_path = Path(ROOT_DIR).joinpath("data/raw/")
+    return pd.read_csv(data_path.joinpath('PD-data-train.csv'), sep=';')
+
+
+def get_test():
+    data_path = Path(ROOT_DIR).joinpath("data/raw/")
+    return pd.read_csv(data_path.joinpath('PD-data-test.csv'), sep=';')
+
+
+def get_data_description():
+    data_path = Path(ROOT_DIR).joinpath("data/raw/")
+    return pd.read_csv(data_path.joinpath('PD-data-desc.csv'), sep=';')
